4b2ac29bb192b2cf015eb95e97482f8fb31931fa
Joe <joehdodd@gmail.com>
2018-03-07 11:04:46 -0500

    setup fetching data on server side, then passing data as props into <App/>

--- src/client/components/App.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import CommentsContainer from './container/CommentsContainer';
-
-const App = props => {
-  return <CommentsContainer />;
-};
-
-export default App;
--- src/server/index.js
+++ src/server/index.js
@@ -2,28 +2,26 @@ import express from 'express';
 import cors from 'cors';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import App from '../client/components/App';
+import App from '../shared/App';
 import Html from '../client/Html';
 import serialize from 'serialize-javascript';
+import { fetchPopularRepos } from '../shared/api';
 
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  /**
-   * renderToString() will take our React app and turn it into a string
-   * to be inserted into our Html template function.
-  **/
-  const name = 'Wiley'
-  const state = serialize(name);
-  const body = renderToString(<App />);
-  res.send(
-    Html({
-      state,
-      body,
-    })
-  );
+  fetchPopularRepos().then(data => {
+    const state = serialize(data);
+    const body = renderToString(<App />);
+    res.send(
+      Html({
+        state,
+        body
+      })
+    );
+  });
 });
 
 app.listen(3000, () => {
